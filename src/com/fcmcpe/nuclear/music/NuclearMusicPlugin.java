b3e86305b07ed92b7f52b7285f653a74ddd8f98e
Snake1999 <237356812@qq.com>
2016-01-17 12:02:54 +0800

    create plugin

---
+++ src/com/fcmcpe/nuclear/music/NuclearMusicPlugin.java
@@ -0,0 +1,8 @@
+package com.fcmcpe.nuclear.music;
+
+/**
+ * Created by Snake1999 on 2016/1/17.
+ * Package com.fcmcpe.nuclear.music in project NuclearMusic.
+ */
+public class NuclearMusicPlugin {
+}
